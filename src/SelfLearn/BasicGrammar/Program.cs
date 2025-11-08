7adb5472cfc13ac993f2b16736bc2fbef1695689
Hiroshi Matsuki <macky1649swd@gmail.com>
2025-11-08 22:31:52 +0900

    BasicGrammar

--- src/SelfLearn/BasicGrammar/Program.cs
+++ src/SelfLearn/BasicGrammar/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Security.AccessControl;
+
+object a = "NHK Trophy";
+
+switch(a)
+{
+    case int n when n > 10:
+        System.Console.WriteLine("Greater than 10");
+        break;
+
+    case string s when s.Length > 5:
+        System.Console.WriteLine("Long string");
+        break;
+}
+
+object obj = "Hello, World!";
+
+Console.WriteLine( obj switch 
+{ 
+    int n when n>10 => "10より大きな整数",
+    int n => "10以下の整数",
+    string s when s.Length > 5 => "長い文字列",
+    string s => "短い文字列",
+    _ => "その他"
+});
+
+var rank = "松";
+
+Console.WriteLine( rank switch
+{
+    "甲" => "最高ランク",
+    "乙" => "中ランク",
+    "丙" => "低ランク",
+    _ => throw new ArgumentException("不明なランクです")
+});
