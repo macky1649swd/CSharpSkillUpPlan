c1fd6482bbfb7d29091fbaa0a216b5ee6c61aecb
Hiroshi Matsuki <macky1649swd@gmail.com>
2025-11-09 12:51:50 +0900

    StandardLib

---
+++ src/SelfLearn/StandardLib/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+var str = "👨‍👩‍👧‍👦";
+
+Console.WriteLine($"👨‍👩‍👧‍👦 str.Length = {str.Length}");
+
+var strInfo = new StringInfo(str);
+
+Console.WriteLine($"👨‍👩‍👧‍👦 StringInfo.LengthInTextElements = {strInfo.LengthInTextElements}");
+
+
+var str1 = "wing";
+var str2 = "WING";
+
+Console.WriteLine($"String Comparison1 {str1.Equals(str2, StringComparison.OrdinalIgnoreCase)}");
+Console.WriteLine($"String Comparison2 {string.Compare(str1, str2, StringComparison.OrdinalIgnoreCase)}");
+
+string? nullableStr = null;
+Console.WriteLine($"IsNullOrEmpty = {string.IsNullOrEmpty(nullableStr)}");
+
+var name = "macky1649";
+Console.WriteLine($"{name} contains digit : {name.Any(ch => Char.IsDigit(ch))}");
+
+
+
+
+
