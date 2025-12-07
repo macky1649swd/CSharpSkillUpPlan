4b4d3380078f6ff585ece48be002b4276a6fd3f1
Hiroshi Matsuki <macky1649swd@gmail.com>
2025-12-07 15:06:52 +0000

    SqlTest (SqlServerはdocker image)

---
+++ src/SelfLearn/SqlTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Data.SqlClient;
+
+var connString =
+    "Server=localhost,1433;Database=master;User Id=sa;Password=Meiwa123;TrustServerCertificate=True;";
+
+using var conn = new SqlConnection(connString);
+
+try
+{
+    conn.Open();
+    Console.WriteLine("SQL Server に接続できました！");
+}
+catch (Exception ex)
+{
+    Console.WriteLine("接続失敗: " + ex.Message);
+}
